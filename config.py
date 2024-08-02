bddcb43d1c13670650f72166e4ff39b71bd7445b
Ankit <155639266+gojo22k@users.noreply.github.com>
2024-08-02 10:43:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ API_ID = os.getenv('API_ID', '25198711')
 API_HASH = os.getenv('API_HASH', '2a99a1375e26295626c04b4606f72752')
 BOT_TOKEN = os.getenv('BOT_TOKEN', '7255118022:AAFehMzJKY0tOanA3ChpEMshrnxn2MPIswE')
 MONGODB_URI = os.getenv('MONGODB_URI', 'mongodb+srv://Aniflix:Lipun123@aniflix.q2wina5.mongodb.net/?retryWrites=true&w=majority&appName=Aniflix')
-IMAGE_URL = 'https://graph.org/file/886790d1aadf63469a316.jpg'
+IMAGE_URL = 'https://graph.org/file/d994f3bdff19e29e8fdca.jpg'
 PAGE_SIZE = 20
